1d5a07f189cf6296704a748a2a50b082400b146c
aburashidakbarov <abdurshd@icloud.com>
2023-06-05 11:25:38 +0900

    added module css to pages

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import styles from './page.module.css'
+
 
 const about = () => {
   return (
-    <div>
+    <div className={styles.container}>
       about
     </div>
   )
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,7 +2,6 @@
 import React from 'react'
 
 const Footer = () => {
-  console.log('Footer')
   return (
     <div>
      <div>
